9a67b9a6b9cdc8e26eb9cf4637e314df0f8eadce
최현식 <85935958+camac0808@users.noreply.github.com>
2022-06-25 05:50:43 +0900

    slideShow

--- slideshow.js
+++ slideshow.js
@@ -1,36 +1,26 @@
 let slides = document.querySelectorAll("#slides img");
+let slideContainer = document.querySelector("#slides");
 let prev = document.getElementById("prev");
 let next = document.getElementById("next");
 let current = 0;
 
-showSlides(current); // 현재 이미지 표시
 prev.addEventListener("click", prevSlide); // 이전 이미지 표시
 next.addEventListener("click", nextSlide); // 다음 이미지 표시
 
 // 현재 이미지 표시 함수
-function showSlides(n) {
-  for (let i=0; i<slides.length; i++) {
-    slides[i].style.display = "none" // 모든 이미지를 숨김
+function showSlides(current) {
+  for (let i = 0; i < slides.length; i++) {
+    slideContainer.style.transition = "0.5s ease-out";
+    slideContainer.style.transform = `translateX(-${1200 * current}px)`;
   }
-  slides[n].style.display = "block" // n번째 이미지만 보임
 }
 
-// 2초마다 슬라이드 자동으로 보여주기
-function intervalSlides(n) {
-  for (let i=0; i<slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  nextSlide(n); // 다음으로 넘긴다
-  setTimeout(intervalSlides, 2000); // 2초뒤에 함수 실행
-}
-intervalSlides(current);
-
-// 이전 이미지 표시 함수 
+// 이전 이미지 표시 함수
 function prevSlide() {
   if (current > 0) {
     current -= 1;
   } else {
-    // 첫번째 위치이므로 (current=0) 마지막으로 보내야된다 
+    // 첫번째 위치이므로 (current=0) 마지막으로 보내야된다
     current = slides.length - 1;
   }
   showSlides(current);
@@ -38,18 +28,25 @@ function prevSlide() {
 
 // 다음 이미지 표시 함수
 function nextSlide() {
-  if (current < slides.length-1) {
+  if (current < slides.length - 1) {
     current += 1;
   } else {
-    // 마지막 위치이므로 (current=2) 처음으로 보내야한다 
+    // 마지막 위치이므로 (current=2) 처음으로 보내야한다
     current = 0;
   }
   showSlides(current);
 }
-console.log(prev, next, slides)
+console.log(prev, next, slides);
 
-
+setInterval(function () {
+  nextSlide();
+  if (current === slides.length - 1) {
+    setTimeout(function () {
+      slideContainer.style.transition = "0s";
+      slideContainer.style.transform = "translateX(0px)";
+    }, 200);
+  }
+}, 1000);
 /* =================================================================
                           자동으로 슬라이드쇼 
 ================================================================*/
-
